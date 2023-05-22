599e2b55b993c21fca21723a2cd612c69f096532
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-05-22 14:09:20 -0700

    next response

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,16 +1,15 @@
 import seed from "../../seed/seed"
-
+import { NextRequest, NextResponse } from 'next/server';
 
 
 export const config = {
   runtime: 'edge', // this is a pre-requisite
 }
 
-const handler = async (req: Request, res: Response) => {
+const handler = async (req: NextRequest, res: NextResponse) => {
   const { url } = await new Response(req.body).json();
-  console.log("URL!!", url)
   await seed(url, 1, process.env.PINECONE_INDEX!, false)
-  return Response.json({ message: "done" })
+  return NextResponse.json({ message: "done" })
 }
 
 export default handler
